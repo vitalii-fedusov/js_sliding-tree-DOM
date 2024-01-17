9b83c62632891aac7ae95f8fdee9a0ae11a4458c
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-01-17 21:05:11 +0200

    make build

---
+++ index.a5dcc362.js
@@ -0,0 +1,3 @@
+var e=document.querySelectorAll("li"),r=document.querySelector(".tree"),n=!0,t=!1,d=void 0;try{for(var l,a=e[Symbol.iterator]();!(n=(l=a.next()).done);n=!0){var i=l.value,o=document.createElement("span");i.prepend(o),o.append(o.nextSibling)}}catch(e){t=!0,d=e}finally{try{n||null==a.return||a.return()}finally{if(t)throw d}}r.addEventListener("click",function(e){var r=e.target;r.parentNode.children[1]&&(r.parentNode.children[1].hidden=!r.parentNode.children[1].hidden)});//# sourceMappingURL=index.a5dcc362.js.map
+
+//# sourceMappingURL=index.a5dcc362.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const liItems = document.querySelectorAll("li");
+const tree = document.querySelector(".tree");
+for (const li of liItems){
+    const span = document.createElement("span");
+    li.prepend(span);
+    span.append(span.nextSibling);
+}
+tree.addEventListener("click", (e)=>{
+    const item = e.target;
+    if (item.parentNode.children[1]) item.parentNode.children[1].hidden = !item.parentNode.children[1].hidden;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
